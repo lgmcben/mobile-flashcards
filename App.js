78b286d51cea0708277600f27fd5cc0de48935dc
droidstick <droidstick@gmail.com>
2018-03-29 18:04:27 +0700

    feat: Display vector icons

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <MaterialCommunityIcons name='cards-outline' color='blue' size={100} />
       </View>
     );
   }
