30dd3abfaf9bd75e0b5e294e097731b9095d9694
droidstick <droidstick@gmail.com>
2018-03-29 18:22:41 +0700

    feat: Create a Deck component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import Deck from './components/Deck'
 
 export default class App extends React.Component {
   componentDidMount() {
@@ -9,21 +10,9 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <MaterialCommunityIcons name='cards-outline' color='blue' size={100} />
+      <View>
+        <Deck />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
