efcae79ce87753b2f7edaca953c47f18944265f5
droidstick <droidstick@gmail.com>
2018-04-20 14:04:23 +0700

    feat: Add TextInput

--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
+
+// New Question View
+// An option to enter in the question
+// An option to enter in the answer
+// An option to submit the new question
 
 export default class NewQuestion extends Component {
+    state = {
+        textQuestion: '',
+        textAnswer: '',
+    }
     render() {
+        const { textQuestion, textAnswer } = this.state
         return (
             <View style={styles.container}>
-                <Text>New Question View</Text>
+                <TextInput>{textQuestion}</TextInput>
             </View>
         )
     }
@@ -13,6 +23,10 @@ export default class NewQuestion extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        padding: 20
+    },
+    textInput: {
+        marginBottom: 20
     }
 })
--- components/Quiz.js
+++ components/Quiz.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 export default class Quiz extends Component {
+
+
+//     Quiz View
+// displays a card question
+// an option to view the answer (flips the card)
+// a "Correct" button
+// an "Incorrect" button
+// the number of cards left in the quiz
+// Displays the percentage correct once the quiz is complete
     render() {
         return (
             <View style={styles.container}>
