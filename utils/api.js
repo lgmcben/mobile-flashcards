520e9ea24c3b7046914d87ef72ba5554e8ce040b
BEER-DELL\lgmcb <droidstick@gmail.com>
2018-04-25 22:07:14 +0700

    chore: Clean up api.js and helpers.js

--- utils/api.js
+++ utils/api.js
@@ -2,68 +2,30 @@ import { AsyncStorage } from 'react-native'
 
 const DECK_STORAGE_KEY = 'my_deck_storage_key'
 
-const tempData = {
-    React: {
-      title: 'React',
-      questions: [
-        {
-          question: 'What is React?',
-          answer: 'A library for managing user interfaces'
-        },
-        {
-          question: 'Where do you make Ajax requests in React?',
-          answer: 'The componentDidMount lifecycle event'
-        }
-      ]
-    },
-    JavaScript: {
-      title: 'JavaScript',
-      questions: [
-        {
-          question: 'What is a closure?',
-          answer: 'The combination of a function and the lexical environment within which that function was declared.'
-        }
-      ]
-    }
-  }
-
 export function getDecks() {
   return AsyncStorage.getItem(DECK_STORAGE_KEY).then((results) => {
     const data = JSON.parse(results)
-    // console.log('getDecks data =', data); 
     const arrayOfDecks = Object.keys(data).map((key) => {
       return {...data[key], id: key}
     });
     return arrayOfDecks;
   });
-
-
-  //     const arrayOfDecks = Object.keys(tempData).map((key) => {
-  //       return {...tempData[key], id: key}
-  //     });
-  // return arrayOfDecks;
 }
 
 export function getDeck(id) {
   return AsyncStorage.getItem(DECK_STORAGE_KEY).then((results) => {
     const data = JSON.parse(results)
-    console.log('get SINGLE Deck, data =', data); 
     return data[id];
   });
 }
 
 export function saveDeckTitle({title='', key=''} = {}) {
-  console.log('saveDeckTitle, title = ', title);
-  console.log('saveDeckTitle, key = ', key);
   return AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({
     [key]: { title, questions: [] }
   }))
-  console.log('saveDeckTitle promise = ', myPromise);
-
 }
 
 export function addCardToDeck({key='', question='', answer=''} = {}) {
-  console.log('addCardToDeck key = ', key);
 
   // Create a card object
   const card = {question: question, answer: answer};
@@ -80,7 +42,6 @@ export function addCardToDeck({key='', question='', answer=''} = {}) {
        [key]: { questions }
      });
 
-     // Merge to storage
      AsyncStorage.mergeItem(DECK_STORAGE_KEY, newDeck);
   });
 }
--- utils/helpers.js
+++ utils/helpers.js
@@ -4,7 +4,6 @@ import { AsyncStorage } from 'react-native'
 const NOTIFICATION_KEY = 'mobileFlashcards:notificaitons'
 
 export function clearLocalNotification () {
-  console.log('clearing notifications');
   return AsyncStorage.removeItem(NOTIFICATION_KEY)
     .then(Notifications.cancelAllScheduledNotificationsAsync)
 }
@@ -25,7 +24,6 @@ function createNotification () {
     }
 }
 
-
 export function setLocalNotification () {
   AsyncStorage.getItem(NOTIFICATION_KEY)
     .then(JSON.parse)
@@ -35,13 +33,10 @@ export function setLocalNotification () {
           .then(({ status }) => {
             if (status === 'granted') {
               Notifications.cancelAllScheduledNotificationsAsync()
-
               let tomorrow = new Date()
               tomorrow.setDate(tomorrow.getDate() + 1)
               tomorrow.setHours(14)
               tomorrow.setMinutes(50)
-              console.log('SETTING notifications');
-
               Notifications.scheduleLocalNotificationAsync(
                 createNotification(),
                 {
@@ -49,151 +44,9 @@ export function setLocalNotification () {
                   repeat: 'day',
                 }
               )
-
               AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
             }
           })
       }
     })
-}
-
-export function getMetricMetaInfo (metric) {
-  const info = {
-    run: {
-      displayName: 'Run',
-      max: 50,
-      unit: 'miles',
-      step: 1,
-      type: 'steppers',
-      getIcon() {
-        return (
-          <View style={[styles.iconContainer, {backgroundColor: red}]}>
-            <MaterialIcons
-              name='directions-run'
-              color={white}
-              size={35}
-            />
-          </View>
-        )
-      }
-    },
-    bike: {
-      displayName: 'Bike',
-      max: 100,
-      unit: 'miles',
-      step: 1,
-      type: 'steppers',
-      getIcon() {
-        return (
-          <View style={[styles.iconContainer, {backgroundColor: orange}]}>
-            <MaterialCommunityIcons
-              name='bike'
-              color={white}
-              size={32}
-            />
-          </View>
-        )
-      }
-    },
-    swim: {
-      displayName: 'Swim',
-      max: 9900,
-      unit: 'meters',
-      step: 100,
-      type: 'steppers',
-      getIcon() {
-        return (
-          <View style={[styles.iconContainer, {backgroundColor: blue}]}>
-            <MaterialCommunityIcons
-              name='swim'
-              color={white}
-              size={35}
-            />
-          </View>
-        )
-      }
-    },
-    sleep: {
-      displayName: 'Sleep',
-      max: 24,
-      unit: 'hours',
-      step: 1,
-      type: 'slider',
-      getIcon() {
-        return (
-          <View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>
-            <FontAwesome
-              name='bed'
-              color={white}
-              size={30}
-            />
-          </View>
-        )
-      }
-    },
-    eat: {
-      displayName: 'Eat',
-      max: 10,
-      unit: 'rating',
-      step: 1,
-      type: 'slider',
-      getIcon() {
-        return (
-          <View style={[styles.iconContainer, {backgroundColor: pink}]}>
-            <MaterialCommunityIcons
-              name='food'
-              color={white}
-              size={35}
-            />
-          </View>
-        )
-      }
-    },
-  }
-
-  return typeof metric === 'undefined'
-    ? info
-    : info[metric]
-}
-
-export function isBetween (num, x, y) {
-  if (num >= x && num <= y) {
-    return true
-  }
-
-  return false
-}
-
-export function calculateDirection (heading) {
-  let direction = ''
-
-  if (isBetween(heading, 0, 22.5)) {
-    direction = 'North'
-  } else if (isBetween(heading, 22.5, 67.5)) {
-    direction = 'North East'
-  } else if (isBetween(heading, 67.5, 112.5)) {
-    direction = 'East'
-  } else if (isBetween(heading, 112.5, 157.5)) {
-    direction = 'South East'
-  } else if (isBetween(heading, 157.5, 202.5)) {
-    direction = 'South'
-  } else if (isBetween(heading, 202.5, 247.5)) {
-    direction = 'South West'
-  } else if (isBetween(heading, 247.5, 292.5)) {
-    direction = 'West'
-  } else if (isBetween(heading, 292.5, 337.5)) {
-    direction = 'North West'
-  } else if (isBetween(heading, 337.5, 360)) {
-    direction = 'North'
-  } else {
-    direction = 'Calculating'
-  }
-
-  return direction
-}
-
-export function timeToString (time = Date.now()) {
-  const date = new Date(time)
-  const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
-  return todayUTC.toISOString().split('T')[0]
 }
